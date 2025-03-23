e7d9036ce12ac96f76e95dc56fdd7e06f3125e49
francesco <francesco.cappa95@gmail.com>
2025-03-23 12:16:01 +0100

    Add PodCounter project from Kubernetes Operators classes

---
+++ operators/project/api/v1alpha1/podcounter_webhook.go
@@ -0,0 +1,223 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+	"fmt"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	validationutils "k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// log is for logging in this package.
+var podcounterlog = logf.Log.WithName("podcounter-resource")
+
+// SetupWebhookWithManager will setup the manager to manage the webhooks
+func (r *PodCounter) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		WithValidator(&PodCounterCustomValidator{}).
+		WithDefaulter(&PodCounterCustomDefaulter{
+			DefaultNamespace: "default",
+		}).
+		Complete()
+}
+
+// +kubebuilder:webhook:path=/mutate-counters-cloudprog-polito-it-v1alpha1-podcounter,mutating=true,failurePolicy=fail,sideEffects=None,groups=counters.cloudprog.polito.it,resources=podcounters,verbs=create;update,versions=v1alpha1,name=mpodcounter.kb.io,admissionReviewVersions=v1
+
+// PodCounterCustomDefaulter struct is responsible for setting default values on the custom resource of the
+// Kind PodCounter when those are created or updated.
+//
+// NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
+// as it is used only for temporary operations and does not need to be deeply copied.
+type PodCounterCustomDefaulter struct {
+	DefaultNamespace string
+}
+
+var _ webhook.CustomDefaulter = &PodCounterCustomDefaulter{}
+
+// Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind podcounter.
+func (d *PodCounterCustomDefaulter) Default(ctx context.Context, obj runtime.Object) error {
+	podcounter, ok := obj.(*PodCounter)
+
+	if !ok {
+		return fmt.Errorf("expected a PodCounter object but got %T", obj)
+	}
+	podcounterlog.Info("Defaulting for PodCounter", "name", podcounter.GetName())
+
+	// Set default values
+	d.applyDefaults(podcounter)
+	return nil
+}
+
+// applyDefaults applies default values to podcounter fields.
+func (d *PodCounterCustomDefaulter) applyDefaults(podcounter *PodCounter) {
+	if podcounter.Spec.Namespace == "" {
+		podcounter.Spec.Namespace = d.DefaultNamespace
+	}
+}
+
+/*
+We can validate our CRD beyond what's possible with declarative
+validation. Generally, declarative validation should be sufficient, but
+sometimes more advanced use cases call for complex validation.
+
+For instance, we'll see below that we use this to validate a well-formed cron
+schedule without making up a long regular expression.
+
+If `webhook.CustomValidator` interface is implemented, a webhook will automatically be
+served that calls the validation.
+
+The `ValidateCreate`, `ValidateUpdate` and `ValidateDelete` methods are expected
+to validate its receiver upon creation, update and deletion respectively.
+We separate out ValidateCreate from ValidateUpdate to allow behavior like making
+certain fields immutable, so that they can only be set on creation.
+ValidateDelete is also separated from ValidateUpdate to allow different
+validation behavior on deletion.
+Here, however, we just use the same shared validation for `ValidateCreate` and
+`ValidateUpdate`. And we do nothing in `ValidateDelete`, since we don't need to
+validate anything on deletion.
+*/
+
+/*
+This marker is responsible for generating a validation webhook manifest.
+*/
+// +kubebuilder:webhook:path=/validate-counters-cloudprog-polito-it-v1alpha1-podcounter,mutating=false,failurePolicy=fail,sideEffects=None,groups=counters.cloudprog.polito.it,resources=podcounters,verbs=create;update,versions=v1alpha1,name=vpodcounter-v1alpha1.kb.io,admissionReviewVersions=v1
+
+// PodCounterCustomValidator struct is responsible for validating the podcounter resource
+// when it is created, updated, or deleted.
+//
+// NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
+// as this struct is used only for temporary operations and does not need to be deeply copied.
+type PodCounterCustomValidator struct {
+}
+
+var _ webhook.CustomValidator = &PodCounterCustomValidator{}
+
+// ValidateCreate implements webhook.CustomValidator so a webhook will be registered for the type PodCounter.
+func (v *PodCounterCustomValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	podcounter, ok := obj.(*PodCounter)
+	if !ok {
+		return nil, fmt.Errorf("expected a PodCounter object but got %T", obj)
+	}
+	podcounterlog.Info("Validation for PodCounter upon creation", "name", podcounter.GetName())
+
+	return nil, validatePodCounter(podcounter)
+}
+
+// ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type podcounter.
+func (v *PodCounterCustomValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	podcounter, ok := newObj.(*PodCounter)
+	if !ok {
+		return nil, fmt.Errorf("expected a PodCounter object for the newObj but got %T", newObj)
+	}
+	podcounterlog.Info("Validation for PodCounter upon update", "name", podcounter.GetName())
+
+	return nil, validatePodCounter(podcounter)
+}
+
+// ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type podcounter.
+func (v *PodCounterCustomValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	podcounter, ok := obj.(*PodCounter)
+	if !ok {
+		return nil, fmt.Errorf("expected a PodCounter object but got %T", obj)
+	}
+	podcounterlog.Info("Validation for PodCounter upon deletion", "name", podcounter.GetName())
+
+	// TODO(user): fill in your validation logic upon object deletion.
+
+	return nil, nil
+}
+
+/*
+We validate the name and the spec of the podcounter.
+*/
+
+// validatePodCounter validates the fields of a podcounter object.
+func validatePodCounter(podcounter *PodCounter) error {
+	var allErrs field.ErrorList
+	if err := validatePodCounterName(podcounter); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if err := validatePodCounterSpec(podcounter); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(
+		schema.GroupKind{Group: "counters.cloudprog.polito.it", Kind: "PodCounter"},
+		podcounter.Name, allErrs)
+}
+
+/*
+Some fields are declaratively validated by OpenAPI schema.
+You can find kubebuilder validation markers (prefixed
+with `// +kubebuilder:validation`) in the
+[Designing an API](api-design.md) section.
+You can find all of the kubebuilder supported markers for
+declaring validation by running `controller-gen crd -w`,
+or [here](/reference/markers/crd-validation.md).
+*/
+
+func validatePodCounterSpec(podcounter *PodCounter) *field.Error {
+	// The field helpers from the kubernetes API machinery help us return nicely
+	// structured validation errors.
+	podcounterlog.Info("Validating spec", "namespace", podcounter.Spec.Namespace)
+	return validateNamespace(podcounter.Spec.Namespace)
+}
+
+func validateNamespace(namespace string) *field.Error {
+	//namespaces cannot contain "kube-" prefix
+	if len(namespace) > 4 && namespace[:5] == "kube-" {
+		return field.Invalid(field.NewPath("spec").Child("namespace"), namespace, "cannot contain 'kube-' prefix")
+	}
+	return nil
+}
+
+/*
+Validating the length of a string field can be done declaratively by
+the validation schema.
+
+But the `ObjectMeta.Name` field is defined in a shared package under
+the apimachinery repo, so we can't declaratively validate it using
+the validation schema.
+*/
+
+func validatePodCounterName(podcounter *PodCounter) *field.Error {
+	if len(podcounter.ObjectMeta.Name) > validationutils.DNS1035LabelMaxLength-11 {
+		// The job name length is 63 characters like all Kubernetes objects
+		// (which must fit in a DNS subdomain). The podcounter controller appends
+		// a 11-character suffix to the podcounter (`-$TIMESTAMP`) when creating
+		// a job. The job name length limit is 63 characters. Therefore podcounter
+		// names must have length <= 63-11=52. If we don't validate this here,
+		// then job creation will fail later.
+		return field.Invalid(field.NewPath("metadata").Child("name"), podcounter.ObjectMeta.Name, "must be no more than 52 characters")
+	}
+	return nil
+}
+
+// +kubebuilder:docs-gen:collapse=Validate object name
---
+++ operators/project/internal/controller/podcounter_controller.go
@@ -0,0 +1,198 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	countersv1alpha1 "cloudprog.polito.it/project/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+const podCounterFinalizer = "finalizer.counters.cloudprog.polito.it"
+
+// PodCounterReconciler reconciles a PodCounter object
+type PodCounterReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=counters.cloudprog.polito.it,resources=podcounters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=counters.cloudprog.polito.it,resources=podcounters/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=counters.cloudprog.polito.it,resources=podcounters/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;
+// +kubebuilder:rbac:groups=core,resources=pods/status,verbs=get
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the PodCounter object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.4/pkg/reconcile
+func (r *PodCounterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// Fetch PodCounter instance
+	var podCounter countersv1alpha1.PodCounter
+	if err := r.Get(ctx, req.NamespacedName, &podCounter); err != nil {
+		log.Error(err, "unable to fetch PodCounter")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	log.Info("Fetched PodCounter", "namespace", podCounter.Spec.Namespace)
+
+	// List all Pods in the specified namespace
+	//wait namespace is set by D
+	var podList corev1.PodList
+	if err := r.List(ctx, &podList, client.InNamespace(podCounter.Spec.Namespace)); err != nil {
+		log.Error(err, "unable to list Pods")
+		return ctrl.Result{}, err
+	}
+
+	// Update status with pod count
+	podCounter.Status.Count = len(podList.Items)
+
+	// Exclude Pods with the label "app=busybox"
+	for _, pod := range podList.Items {
+		if hasBusyboxLabel(&pod) {
+			podCounter.Status.Count--
+		}
+	}
+
+	if err := r.Status().Update(ctx, &podCounter); err != nil {
+		log.Error(err, "unable to update PodCounter status")
+		return ctrl.Result{}, err
+	}
+
+
+	// Handle deletion (Finalizer Logic)
+	if !podCounter.DeletionTimestamp.IsZero() {
+		// Resource is being deleted
+		if controllerutil.ContainsFinalizer(&podCounter, podCounterFinalizer) {
+			// Perform cleanup tasks
+			log.Info("Performing finalizer cleanup", "PodCounter", podCounter.Name)
+			if err := r.cleanupPodCounter(ctx, &podCounter); err != nil {
+				log.Error(err, "Failed to clean up PodCounter")
+				return ctrl.Result{}, err
+			}
+
+			// Remove finalizer after cleanup
+			controllerutil.RemoveFinalizer(&podCounter, podCounterFinalizer)
+			if err := r.Update(ctx, &podCounter); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// Ensure the finalizer is added
+	if !controllerutil.ContainsFinalizer(&podCounter, podCounterFinalizer) {
+		controllerutil.AddFinalizer(&podCounter, podCounterFinalizer)
+		if err := r.Update(ctx, &podCounter); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	log.Info("Updated PodCounter", "namespace", podCounter.Spec.Namespace, "count", podCounter.Status.Count)
+	return ctrl.Result{}, nil
+}
+
+// Cleanup logic when deleting a PodCounter
+func (r *PodCounterReconciler) cleanupPodCounter(ctx context.Context, podCounter *countersv1alpha1.PodCounter) error {
+	// Example cleanup: Log that we're cleaning up
+	log := log.FromContext(ctx)
+	log.Info("Cleaning up resources for PodCounter", "name", podCounter.Name)
+
+	// If there were external resources (e.g., deleting a ConfigMap), we would remove them here
+
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *PodCounterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&countersv1alpha1.PodCounter{}).
+		Watches(
+			&corev1.Pod{}, // This logic works in controller-runtime v0.15+
+			handler.EnqueueRequestsFromMapFunc(func(ctx context.Context, obj client.Object) []reconcile.Request {
+				_, ok := obj.(*corev1.Pod)
+				if !ok {
+					return nil
+				}
+
+				// Find associated PodCounter (adjust logic if needed)
+				podCounterList := &countersv1alpha1.PodCounterList{}
+				err := r.Client.List(ctx, podCounterList)
+				if err != nil {
+					return nil
+				}
+
+				// Enqueue reconcile requests
+				var requests []reconcile.Request
+				for _, pc := range podCounterList.Items {
+					requests = append(requests, reconcile.Request{
+						NamespacedName: types.NamespacedName{Name: pc.Name, Namespace: pc.Namespace},
+					})
+				}
+				return requests
+			}),
+			builder.WithPredicates(
+				predicate.Funcs{
+					CreateFunc: func(e event.CreateEvent) bool {
+						// Only process Pods that DO NOT have the label "app=busybox"
+						return !hasBusyboxLabel(e.Object)
+					},
+					UpdateFunc: func(e event.UpdateEvent) bool {
+						// Process updates only if the Pod DOES NOT have "app=busybox"
+						return !hasBusyboxLabel(e.ObjectNew)
+					},
+					DeleteFunc: func(e event.DeleteEvent) bool {
+						// Process deletions only if the Pod DID NOT have "app=busybox"
+						return !hasBusyboxLabel(e.Object)
+					},
+					GenericFunc: func(e event.GenericEvent) bool {
+						// Ignore generic events
+						return false
+					},
+				}),
+		).
+		Complete(r)
+}
+
+
+// Helper function to check if a Pod has the "app=busybox" label
+func hasBusyboxLabel(obj client.Object) bool {
+	if obj == nil {
+		return false
+	}
+	labels := obj.GetLabels()
+	val, exists := labels["app"]
+	return exists && val == "busybox"
+}
